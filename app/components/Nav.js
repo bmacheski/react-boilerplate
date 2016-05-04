ae4c7661ec9223594e5f078931781f4cb3ce3b02
Brian Macheski <bwm0007@gmail.com>
2016-05-04 13:35:25 -0500

    Fix nav indentation

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-    <div>
+      <div>
         <h1>Nav</h1>
       </div>
     );
